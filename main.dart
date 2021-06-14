e43cb29440d8c802fbe12d0d0d4a116c247c52a5
kenennacc <>
2021-06-14 20:45:26 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,33 @@
+import "dart:math";
+import "dart:io";
+
+String prompt(String promptText){
+  print("The prompt is: ${promptText}");
+  String answer = stdin.readLineSync();
+  return answer;
+}
+
+double promptDouble(){
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync());
+  return myNum;
+}
+
+void main() {
+  
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("Enter an Operator (+,-,*,/):");
+
+if(op == '+'){ 
+  print(num1 + num2);
+} else if(op == '-') {
+  print(num1 - num2);
+  } else if(op == '/') {
+  print(num1 / num2);
+} else if(op == '*') {
+  print(num1 * num2);
+} else {
+  print("invalid operator!");
+}
+}
